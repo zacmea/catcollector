557e7a9e465f3dd3ce9ba5a82684fe10222ee92b
zacmea <zach.mears@gmail.com>
2024-05-11 15:07:09 -0400

    adds cats index page. puts footer at bottom

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), # this is the home route, the same as '/'. the name arg is a keyword arg that we can use to refer to this path in other parts of our code
     path('about/', views.about, name='about'), # this is the about route, the same as '/about/'
+    path('cats/', views.cats_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+  {'name': 'Tom', 'breed': 'Tom', 'description': 'gentle and loving', 'age': 0},
+]
 # Create your views here.
 def home(request):
     return render(request, 'home.html',) # this is the home route, the same as '/'. the name arg is a keyword arg that we can use to refer to this path in other parts of our code
 
 def about(request):
     return render(request, 'about.html',) # this is the about route, the same as '/about/'
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {'cats': cats}) # this is the cats route, the same as '/cats/'
