f351ec1c83ac26dede5ed75aaaf812a1848c5a34
zacmea <zach.mears@gmail.com>
2024-05-11 14:43:16 -0400

    homepage set up. base.html, static css

---
+++ main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'), # this is the home route, the same as '/'. the name arg is a keyword arg that we can use to refer to this path in other parts of our code
+    path('about/', views.about, name='about'), # this is the about route, the same as '/about/'
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html',) # this is the home route, the same as '/'. the name arg is a keyword arg that we can use to refer to this path in other parts of our code
+
+def about(request):
+    return render(request, 'about.html',) # this is the about route, the same as '/about/'
